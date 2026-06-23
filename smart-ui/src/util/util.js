a721a8a64e95d8ae14048d267e9fb094db06a3da
JnyRoad <jnyroad@gmail.com>
2026-06-22 20:44:07 -0700

    refactor(smart-ui): split theme/crypto/fullscreen/dom utils out of util.js

--- smart-ui/src/util/util.js
+++ smart-ui/src/util/util.js
@@ -1,123 +1,13 @@
 import {validatenull} from './validate'
-import request from '@/router/axios'
 
-// 以下对象/数组/类型判断类纯函数已拆分到 ./object，统一 re-export，
-// 保证 @/util/util 对外公共面与调用方完全不变。
+// util.js 已按领域拆分为独立模块，这里统一 re-export，保证 @/util/util 对外公共面与调用方完全不变。
 export { serialize, getObjType, deepClone, diff, isObjectValueEqual, isArrayFn } from './object'
-// 数值处理类纯函数已拆分到 ./number，同样 re-export。
 export { randomLenNum, formatNumber, getProportion, floatNumMinus } from './number'
-/**
- * 设置灰度模式
- */
-export const toggleGrayMode = (status) => {
-  if (status) {
-    document.body.className = document.body.className + ' grayMode'
-  } else {
-    document.body.className = document.body.className.replace(' grayMode', '')
-  }
-}
-/**
- * 设置主题
- */
-export const setTheme = (name) => {
-  document.body.className = name
-}
+export { toggleGrayMode, setTheme } from './theme'
+export { encryption } from './crypto'
+export { fullscreenToggel, listenfullscreen, fullscreenEnable, reqFullScreen, exitFullScreen } from './fullscreen'
+export { loadStyle, openWindow, handleImg } from './dom'
 
-/**
- *加密处理
- */
-export const encryption = (params) => {
-  let {
-    data,
-    type,
-    param,
-    key
-  } = params
-  const result = JSON.parse(JSON.stringify(data))
-  if (type === 'Base64') {
-    param.forEach(ele => {
-      result[ele] = btoa(result[ele])
-    })
-  } else {
-    param.forEach(ele => {
-      var data = result[ele]
-      key = CryptoJS.enc.Latin1.parse(key)
-      var iv = key
-      // 加密
-      var encrypted = CryptoJS.AES.encrypt(
-        data,
-        key, {
-          iv: iv,
-          mode: CryptoJS.mode.CBC,
-          padding: CryptoJS.pad.ZeroPadding
-        })
-      result[ele] = encrypted.toString()
-    })
-  }
-  return result
-}
-
-/**
- * 浏览器判断是否全屏
- */
-export const fullscreenToggel = () => {
-  if (fullscreenEnable()) {
-    exitFullScreen();
-  } else {
-    reqFullScreen();
-  }
-};
-/**
- * esc监听全屏
- */
-export const listenfullscreen = (callback) => {
-  function listen() {
-    callback()
-  }
-  document.addEventListener("fullscreenchange", function () {
-    listen();
-  });
-  document.addEventListener("mozfullscreenchange", function () {
-    listen();
-  });
-  document.addEventListener("webkitfullscreenchange", function () {
-    listen();
-  });
-  document.addEventListener("msfullscreenchange", function () {
-    listen();
-  });
-};
-/**
- * 浏览器判断是否全屏
- */
-export const fullscreenEnable = () => {
-  return document.isFullScreen || document.mozIsFullScreen || document.webkitIsFullScreen
-}
-
-/**
- * 浏览器全屏
- */
-export const reqFullScreen = () => {
-  if (document.documentElement.requestFullScreen) {
-    document.documentElement.requestFullScreen();
-  } else if (document.documentElement.webkitRequestFullScreen) {
-    document.documentElement.webkitRequestFullScreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  }
-};
-/**
- * 浏览器退出全屏
- */
-export const exitFullScreen = () => {
-  if (document.documentElement.requestFullScreen) {
-    document.exitFullScreen();
-  } else if (document.documentElement.webkitRequestFullScreen) {
-    document.webkitCancelFullScreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.mozCancelFullScreen();
-  }
-};
 /**
  * 递归寻找子类的父类
  */
@@ -138,18 +28,6 @@ export const findParent = (menu, id) => {
   }
 }
 
-/**
- * 动态插入css
- */
-
-export const loadStyle = url => {
-  const link = document.createElement('link')
-  link.type = 'text/css'
-  link.rel = 'stylesheet'
-  link.href = url
-  const head = document.getElementsByTagName('head')[0]
-  head.appendChild(link)
-}
 /**
  * 根据字典的value显示label
  */
@@ -189,45 +67,6 @@ export const findArray = (dic, value) => {
     }
   }
   return -1
-}
-/**
- * 打开小窗口
- */
-export const openWindow = (url, title, w, h) => {
-  // Fixes dual-screen position                            Most browsers       Firefox
-  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left
-  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top
-
-  const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width
-  const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height
-
-  const left = ((width / 2) - (w / 2)) + dualScreenLeft
-  const top = ((height / 2) - (h / 2)) + dualScreenTop
-  const newWindow = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left)
-
-  // Puts focus on the newWindow
-  if (window.focus) {
-    newWindow.focus()
-  }
-}
-
-/**
- *  <img> <a> src 处理
- * @returns {PromiseLike<T | never> | Promise<T | never>}
- */
-export function handleImg(fileName, id) {
-  return validatenull(fileName)?null: request({
-    url: '/admin/file/' + fileName,
-    method: 'get',
-    responseType: 'blob'
-  }).then((response) => { // 处理返回的文件流
-    let blob = response.data;
-    let img = document.getElementById(id);
-    img.src = URL.createObjectURL(blob);
-    window.setTimeout(function () {
-      window.URL.revokeObjectURL(blob)
-    }, 0)
-  })
 }
 
 export function dateFormat(date = new Date(), fmt = 'yyyy-MM-dd') {
